bcbf21071143e208663e4279a0da8541531bf664
MilanKovacic <MilanKovacic@users.noreply.github.com>
2023-08-18 22:58:48 +0200

    feat: add sourcemap (#24)
    * feat: add sourcemap
    
    * chore: add changeset

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         dts({ rollupTypes: true }),
       ],
   build: {
+    sourcemap: true,
     lib: {
       // eslint-disable-next-line unicorn/prefer-module
       entry: resolve(__dirname, "src/index.ts"),
